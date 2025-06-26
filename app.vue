d9214a558cc7538e34708e456811b79b303fc3d8
John Criste <ionutcriste28@gmail.com>
2025-06-26 17:57:13 +0100

    Init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+useHead({
+  meta: [
+    { name: 'description', content: 'This is the home page of our Nuxt application.' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ]
+})
+</script>
+
+<template>
+<div>
+  <NuxtPage />
+</div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    hello projects
+  </div>
+
+</template>
